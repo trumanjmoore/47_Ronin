b652926bfbf232fb3a0b989cbf5116636c3f9d86
Kingslayer <williamleva@live.com>
2026-02-10 19:09:29 -0500

    Test commit

--- Godot Project/scripts/character_ronin.gd
+++ Godot Project/scripts/character_ronin.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 @export var speed = 100
 @export var jump_speed = -100
 @export var gravity = 500
+var combo_count: int = 0
+@onready var combo_timer: Timer = $ComboTimer
 
 
 func _process(delta: float) -> void:
@@ -28,3 +30,21 @@ func _physics_process(delta):
 	# Only allow jumping when on the ground
 	if Input.is_action_just_pressed("space") and is_on_floor():
 		velocity.y = jump_speed
+	
+#func _input(event):
+	#if event.is_action_just_pressed("light_attack"):
+	#		handle_combo_input()
+
+func handle_combo_input():
+	if combo_timer.is_stopped():
+		# Start the first attack
+		combo_count = 1
+		#play_attack_animation("Attack_1")
+		combo_timer.start()
+	elif combo_count == 1:
+		#play_attack_animation("Attack_2")
+		combo_timer.start()
+	elif combo_count == 2:
+		combo_count == 3
+		#play_attack_animation("Attack_3")
+		combo_timer.start()
