143b98dbd37ddea7a8e477f340a3d5485d7124af
Truman Moore <tj.moore@ufl.edu>
2026-02-16 23:45:44 -0500

    added jumping, up attack, fixed some animattions

--- Godot Project/scripts/character_ronin.gd
+++ Godot Project/scripts/character_ronin.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 @export var speed = 60
-@export var jump_speed = -100
+@export var jump_speed = -200
 @export var gravity = 500
 var combo_count: int = 0
 @onready var combo_timer: Timer = $ComboTimer
@@ -10,10 +10,15 @@ var attack_index = 0
 var sheathing = false
 var timedout = false
 var attacking = false
+var jumping = false
 
 func _process(delta: float) -> void:
-	if !sheathing:
+	if !sheathing and !jumping:
 		if Input.is_action_just_pressed("attack"):
+			if Input.is_action_pressed("ui_up"):
+				$AnimatedSprite2D.play("attack_up")
+				attack_index = 4
+				attacking = true
 			match attack_index:
 				0:
 					$AnimatedSprite2D.play("attack_one")
@@ -31,26 +36,29 @@ func _process(delta: float) -> void:
 						attack_index = 3
 						attacking = true
 						$ComboTimer.stop()
-		elif attack_index == 0:
-			if velocity.x < 0:
-				$AnimatedSprite2D.flip_h = true
-				$AnimatedSprite2D.play("walking")
-			elif velocity.x > 0:
-				$AnimatedSprite2D.flip_h = false
-				$AnimatedSprite2D.play("walking")
-			else:
-				$AnimatedSprite2D.play("breathing")
+		elif !attacking and attack_index == 0:
+			if Input.is_action_just_pressed("space"):
+				$AnimatedSprite2D.play("jump")
+				jumping = true
+			elif !jumping:
+				if velocity.x < 0:
+					$AnimatedSprite2D.flip_h = true
+					$AnimatedSprite2D.play("walking")
+				elif velocity.x > 0:
+					$AnimatedSprite2D.flip_h = false
+					$AnimatedSprite2D.play("walking")
+				else:
+					$AnimatedSprite2D.play("breathing")
 	
 	
 
 func _physics_process(delta):
-	# Add gravity every frame
-	velocity.y += gravity * delta
 
-	# Input affects x axis only
-	velocity.x = Input.get_axis("ui_left", "ui_right") * speed
-
-	move_and_slide()
+	if !attacking and attack_index == 0:
+		# Add gravity every frame
+		velocity.y += gravity * delta
+		velocity.x = Input.get_axis("ui_left", "ui_right") * speed
+		move_and_slide()
 
 	# Only allow jumping when on the ground
 	if Input.is_action_just_pressed("space") and is_on_floor():
@@ -64,9 +72,11 @@ func _sheath() -> void:
 	attacking = false
 
 func _on_animated_sprite_2d_animation_finished() -> void:
-	if sheathing == true:
+	if sheathing:
 		sheathing = false
-	elif attack_index == 3:
+	elif jumping:
+		jumping = false
+	elif attack_index > 2:
 		_sheath()
 	elif attack_index > 0:
 		$ComboTimer.start(.2)
